d4c9d987559917fde5df610ffeed2ce8cb579510
ntlong12345 <longnthe172278@fpt.edu.vn>
2024-05-29 23:41:34 +0700

    Create checkAdd.java

---
+++ src/java/controller/manage/checkAdd.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.manage;
+
+import dal.ProductsDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class checkAdd extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet checkAdd</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet checkAdd at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String cid = request.getParameter("cid");
+        session.setAttribute("productAdd", cid);
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    } 
+    
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String name = request.getParameter("name");
+        String quantity = request.getParameter("quantity");
+        String price = request.getParameter("price");
+        String describe = request.getParameter("describe");
+        String image = request.getParameter("image");
+        String cid = request.getParameter("cid");
+
+        int cid_int, quantity_int;
+        float price_float;
+        try {
+            cid_int = Integer.parseInt(cid);
+            quantity_int = Integer.parseInt(quantity);
+            price_float = Float.parseFloat(price);
+            ProductsDAO db = new ProductsDAO();
+
+            db.addProduct(name, quantity_int, price_float, describe, "images/" + image, cid_int);
+
+            session.removeAttribute("productAdd");
+
+        } catch (NumberFormatException e) {
+
+        }
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
