32a7dfddcd1c65ffb6efcfa4c44694d1bf089c47
XuanHung <hungtxhe171405@fpt.edu.vn>
2024-06-08 22:56:02 +0700

    Update StatusDAO.java

--- src/java/dal/StatusDAO.java
+++ src/java/dal/StatusDAO.java
@@ -3,11 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
-
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Status;
 /**
  *
  * @author HP
  */
-public class StatusDAO {
-    
+public class StatusDAO extends DBContext{
+    public Status getStatusById(int id){
+        String sql= "select * from Status where id = ?";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                Status status = new Status(rs.getInt("id"), rs.getString("nameStatus"));
+                
+                return status;
+            }
+        } catch (SQLException e) {
+        }
+        return null;
+    }
 }
