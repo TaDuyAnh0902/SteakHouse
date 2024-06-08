24ae86b89516ad107208b902bb9358910b3a63a9
minhkhai10122003 <khainmhe171207@fpt.edu.vn>
2024-06-08 23:23:27 +0700

    Update Home.java

--- src/java/controller/Home.java
+++ src/java/controller/Home.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import dal.AccountDAO;
 import dal.ProductsDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,6 +15,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Random;
+import model.Category;
 import model.Product;
 
 /**
@@ -22,34 +24,37 @@ import model.Product;
  * @author Admin
  */
 public class Home extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet Home</title>");  
+            out.println("<title>Servlet Home</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet Home at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet Home at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -57,38 +62,62 @@ public class Home extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       HttpSession session = request.getSession();
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
         ProductsDAO PdDAO = new ProductsDAO();
+        AccountDAO acDAO = new AccountDAO();
         String idTable = request.getParameter("idTable");
-        
-        if(idTable!=null){
+        if (session.getAttribute("role") == null) {
+            session.setAttribute("role", 3);
+        }
+        if (idTable != null) {
             try {
                 int idTable_int = Integer.parseInt(idTable);
                 session.setAttribute("tableNumber", idTable_int);
-                
+
+                String randomString = generateRandomString(10);
+                session.setAttribute("userByMobile", randomString);
+                acDAO.addUserPhone(randomString, 4);
+
             } catch (NumberFormatException e) {
             }
         }
-//        if (session.getAttribute("role") == null) {
-//            response.sendRedirect("login");
-//        }
-//
-//        if (session.getAttribute("role") != null) {
-//            if ((int) session.getAttribute("role") == 3) {
-//                response.sendRedirect("GetListTable");
-//            } else {
+        switch ((int) session.getAttribute("role")) {
+            case 1 -> {
+                session.setAttribute("manage", "success");
+                request.setAttribute("statistics", "success");
+                List<Category> c = PdDAO.getAllCategory();
+                session.setAttribute("data", c);
+            }
+            case 2 ->
+                request.setAttribute("manageOrder", "success");
+            default -> {
                 request.setAttribute("main", "success");
                 List<Product> list = PdDAO.getNewProduct();
-                
                 session.setAttribute("getNewProduct", list);
-                request.getRequestDispatcher("home.jsp").forward(request, response);
-//            }
-//        }
-    } 
+            }
+        }
 
-    /** 
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+
+    }
+
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -96,12 +125,13 @@ public class Home extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
