ea4d582c10ab6606f36a14b19817a11f0fbdb26e
ntlong12345 <longnthe172278@fpt.edu.vn>
2024-06-13 00:13:57 +0700

    Update OrderDAO.java

--- src/java/dal/OrderDAO.java
+++ src/java/dal/OrderDAO.java
@@ -10,13 +10,18 @@ import model.Table;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import model.Account;
+import model.OrderLine;
+import model.Product;
 import model.Status;
+
 /**
  *
  * @author ASUS
  */
 public class OrderDAO extends ProductsDAO {
-   public List<Table> getAllTable() {
+
+    public List<Table> getAllTable() {
         String sql = "select * from [Table]";
         List<Table> list = new ArrayList<>();
 
@@ -36,13 +41,13 @@ public class OrderDAO extends ProductsDAO {
         return list;
     }
 
-    public Table getTableById(int id){
+    public Table getTableById(int id) {
         String sql = "select * from [Table] where id = ?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 Table tb = new Table();
                 tb.setId(rs.getInt("id"));
                 tb.setNameTable(rs.getString("nameTable"));
@@ -54,19 +59,20 @@ public class OrderDAO extends ProductsDAO {
         }
         return null;
     }
-    public Table deleteTableById(int id){
+
+    public Table deleteTableById(int id) {
         String sql = "Update [Table] set sid = 2 where id = ?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, id);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
         return null;
     }
-    
-    public void addTable(String nameTable){
+
+    public void addTable(String nameTable) {
         String sql = """
                      INSERT INTO [dbo].[Table]
                                 ([nameTable], sid)
@@ -76,12 +82,12 @@ public class OrderDAO extends ProductsDAO {
             PreparedStatement st = connection.prepareStatement(sql);
             st.setString(1, nameTable);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
     }
-    
-    public void updateTable(String nameTable, int id){
+
+    public void updateTable(String nameTable, int id) {
         String sql = """
                      UPDATE [dbo].[Table]
                         SET [nameTable] = ?
@@ -91,8 +97,110 @@ public class OrderDAO extends ProductsDAO {
             st.setString(1, nameTable);
             st.setInt(2, id);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
+    }
+
+    public void addOrderLine(int productId, int quantity, String user, int tableNumber) {
+        String sql = """
+                     SET IDENTITY_INSERT [dbo].[OrderLine] OFF;
+                              
+                              INSERT INTO [dbo].[OrderLine] ([pid], [quantity], [aid], [dateOrderline], [tid], sid) 
+                              VALUES
+                              (?,?,?,CURRENT_TIMESTAMP,?,1)
+                              
+                              SET IDENTITY_INSERT [dbo].[OrderLine] ON;""";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, productId);
+            st.setInt(2, quantity);
+            st.setString(3, user);
+            st.setInt(4, tableNumber);
+            st.executeUpdate();
+        } catch (SQLException e) {
+        }
+    }
+
+    public List<OrderLine> getListOrderLine(String user) {
+        String sql = """
+                    SELECT pid, aid, tid, sid, MAX(quantity) AS max_quantity
+                    FROM orderline
+                    GROUP BY pid, aid, tid, sid
+                    HAVING aid = ?
+                 """;
+        List<OrderLine> list = new ArrayList<>();
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, user);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                OrderLine ol = new OrderLine();
+                // Since the `id` column is not part of the grouped result, you might need to handle it differently
+// ol.setId(rs.getInt("id"));
+
+                Product p = getProductById(rs.getString("pid"));
+                ol.setPid(p);
+                Account ac = check(rs.getString("aid"));
+                ol.setAid(ac);
+                Table t = getTableById(rs.getInt("tid"));
+                ol.setTid(t);
+                Status s = getStatusById(rs.getInt("sid"));
+                ol.setSid(s);
+                ol.setQuantity(rs.getInt("max_quantity"));
+                list.add(ol);
+            }
+        } catch (SQLException e) {
+            // Better to log the exception
+
+        }
+
+        return list;
+    }
+
+    public void buy(String user) {
+        String sql = """
+                     UPDATE [dbo].[OrderLine]
+                      SET [sid] = 2
+                      WHERE [aid] = ? 
+                      AND [quantity] = (
+                          SELECT MAX([quantity])
+                          FROM [dbo].[OrderLine]
+                          WHERE [aid] = ?
+                      );""";
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, user);
+            st.setString(2, user);
+            st.executeUpdate();
+        } catch (SQLException e) {
+        }
+    }
+
+    public double totalMoney(String user) {
+        String sql = """
+                    SELECT pid, aid, tid, sid, MAX(quantity) AS max_quantity
+                    FROM orderline
+                    GROUP BY pid, aid, tid, sid
+                    HAVING aid = ?
+                 """;
+        double total = 0;
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, user);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                OrderLine ol = new OrderLine();
+                Product p = getProductById(rs.getString("pid"));
+                ol.setQuantity(rs.getInt("max_quantity"));
+
+                total += p.getPrice() * ol.getQuantity();
+            }
+            st.executeUpdate();
+        } catch (SQLException e) {
+        }
+
+        return total;
     }
 }
