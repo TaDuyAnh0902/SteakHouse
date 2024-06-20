daa6df952c5b37fd113a8dba27e67fa206933e00
光明 <minhcqhe187164@fpt.edu.vn>
2024-06-21 00:58:30 +0700

    Update LogInLogOut.java

--- src/java/controller/LogIn/LogInLogOut.java
+++ src/java/controller/LogIn/LogInLogOut.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.LogIn;
 
 import java.io.IOException;
@@ -19,34 +18,37 @@ import jakarta.servlet.http.HttpSession;
  * @author ASUS
  */
 public class LogInLogOut extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LogInLogOut</title>");  
+            out.println("<title>Servlet LogInLogOut</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LogInLogOut at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LogInLogOut at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -54,24 +56,29 @@ public class LogInLogOut extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
-                
+
         String check = request.getParameter("check");
-        if(check.equals("logOut")){
+        if (check.equals("logOut")) {
             session.removeAttribute("role");
             session.removeAttribute("success");
             session.removeAttribute("manage");
             session.removeAttribute("manageOrder");
             session.removeAttribute("productsManagement");
+            session.removeAttribute("accountManagement");
+            session.removeAttribute("tableManagement");
+            session.removeAttribute("blogManagement");
+            session.removeAttribute("allListTable");
             response.sendRedirect("home");
-        }else if(check.equals("logIn")){
+        } else if (check.equals("logIn")) {
             response.sendRedirect("login");
         }
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -79,12 +86,13 @@ public class LogInLogOut extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
