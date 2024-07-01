51452a0264c49393c9c38f1941c6917c0f8b268e
XuanHung <hungtxhe171405@fpt.edu.vn>
2024-07-01 22:46:07 +0700

    Update pay.java

--- src/java/Order/pay.java
+++ src/java/Order/pay.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Order;
 
+import dal.HomeMobileRequestDAO;
 import dal.OrderDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,40 +15,44 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.util.List;
 import model.OrderLine;
+import model.PaymentRequest;
 
 /**
  *
  * @author HP
  */
 public class pay extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet pay</title>");  
+            out.println("<title>Servlet pay</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet pay at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet pay at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,28 +60,32 @@ public class pay extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
         OrderDAO odDAO = new OrderDAO();
         String idTable = request.getParameter("idTable");
         String totalMoney = request.getParameter("totalMoney");
-        
+        HomeMobileRequestDAO hmDAO = new HomeMobileRequestDAO();
         int idTable_int;
         double totalMoney_double;
         try {
             idTable_int = Integer.parseInt(idTable);
-            totalMoney_double = Double.parseDouble(totalMoney);
-            odDAO.pay(idTable_int, totalMoney_double);
+            odDAO.pay(idTable_int, totalMoney);
+            hmDAO.deletePaymentRequest(idTable_int);
+            hmDAO.deleteClientRequest(idTable_int);
         } catch (Exception e) {
         }
-        session.removeAttribute("allListTable");
+        session.removeAttribute("tableStatus");
         List<OrderLine> list = odDAO.getAllListOrderLine();
         session.setAttribute("allListOrderLine", list);
-        request.getRequestDispatcher("home.jsp").forward(request, response);
-    } 
+        List<PaymentRequest> list2 = hmDAO.getAllPaymentRequest();
+        session.setAttribute("listPaymentRequest", list2);
+        response.sendRedirect("manageOrderAction?check=viewOrder");
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -85,12 +93,13 @@ public class pay extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
