fac3fc812014336786a63f33dbe1af2a8394e1f9
minhkhai10122003 <khainmhe171207@fpt.edu.vn>
2024-07-04 00:27:00 +0700

    Create SendEmail.java

---
+++ src/java/controller/LogIn/SendEmail.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.LogIn;
+import java.util.Properties;
+import java.util.Random;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import model.Account;
+/**
+ *
+ * @author ASUS
+ */
+public class SendEmail {
+
+    public String getRandom() {
+        Random rnd = new Random();
+        int number = rnd.nextInt(999999);
+        return String.format("%06d", number);
+    }
+
+    public boolean sendEmail(Account user) {
+        boolean test = false;
+        String toEmail = user.getEmail();
+        String fromEmail = "steakhouseswp@gmail.com";
+        String password = "syup vpum sefq dmoj";
+
+        try {
+            Properties pr = new Properties();
+            pr.setProperty("mail.smtp.host", "smtp.gmail.com");
+            pr.setProperty("mail.smtp.port", "587");
+            pr.setProperty("mail.smtp.auth", "true");
+            pr.setProperty("mail.smtp.starttls.enable", "true");
+//            pr.put("mail.smtp.socketFactory.port", "587");
+//            pr.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+            Session session = Session.getInstance(pr, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(fromEmail, password);
+                }
+            });
+
+            Message mess = new MimeMessage(session);
+            mess.setFrom(new InternetAddress(fromEmail));
+            mess.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            mess.setSubject("User Email Verification");
+            mess.setText("Registered successfully. Please verify your account using this code:" + user.getCode());
+
+            Transport.send(mess);
+            test = true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return test;
+    }
+}
