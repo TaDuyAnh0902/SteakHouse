cc5b5322186c6af1a94b345bb447a70143792eec
minhkhai10122003 <khainmhe171207@fpt.edu.vn>
2024-07-04 16:43:11 +0700

    Create changeProfile.java

---
+++ src/java/controller/LogIn/changeProfile.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.LogIn;
+
+import dal.AccountDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Account;
+
+/**
+ *
+ * @author ASUS
+ */
+public class changeProfile extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet changeProfile</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet changeProfile at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setAttribute("profileChange", "success");
+        String user = request.getParameter("user");
+
+        AccountDAO db = new AccountDAO();
+
+        Account ac = db.getAccountByUser(user);
+
+        request.setAttribute("profileNew", ac);
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String user = request.getParameter("user");
+        String phone = request.getParameter("phoneNumber");
+        String pass = request.getParameter("pass");
+        String email = request.getParameter("email");
+        String confirmPass = request.getParameter("confirmPass");
+
+        AccountDAO db = new AccountDAO();
+
+        if (pass.equals(confirmPass)) {
+            db.updateUser(name, user, phone, pass);
+            Account ac = db.getAccountByUser(user);
+            request.setAttribute("profile", ac);
+            request.setAttribute("successfully", "Registered successfully");
+        } else {
+            request.setAttribute("profileChange", "success");
+
+            Account ac = db.getAccountByUser(user);
+
+            request.setAttribute("profileNew", ac);
+            request.setAttribute("passwordNotMatch", "passwords do not match");
+        }
+
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
