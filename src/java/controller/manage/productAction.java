a2ff251c17d201a1bf4d0d963fc25f7539962097
ntlong12345 <longnthe172278@fpt.edu.vn>
2024-07-05 21:39:04 +0700

    Update productAction.java

--- src/java/controller/manage/productAction.java
+++ src/java/controller/manage/productAction.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.manage;
 
 import dal.ProductsDAO;
@@ -12,6 +11,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import model.Product;
 
 /**
@@ -19,34 +19,37 @@ import model.Product;
  * @author ADMIN
  */
 public class productAction extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet productAction</title>");  
+            out.println("<title>Servlet productAction</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet productAction at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet productAction at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,20 +59,23 @@ public class productAction extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String id = request.getParameter("id");
-
+        String cid = request.getParameter("cid");
         String action = request.getParameter("action");
         ProductsDAO db = new ProductsDAO();
         if ("delete".equals(action)) {
             db.deleteProductById(id);
-
+            int cid_int;
+            try {
+                cid_int = Integer.parseInt(cid);
+                List<Product> list = db.getproductByCid(cid_int);
+                request.setAttribute("products", list);
+            } catch (NumberFormatException e) {
+            }
         } else if ("edit".equals(action)) {
-            String cid = request.getParameter("cid");
             Product productUpdate = db.getProductById(id);
-            request.setAttribute("cid", cid);
             request.setAttribute("productUpdate", productUpdate);
-
         }
-        response.sendRedirect("manageOption?check=productsManagement");
+        request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 
     /**
@@ -106,8 +112,9 @@ public class productAction extends HttpServlet {
         request.getRequestDispatcher("home.jsp").forward(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
