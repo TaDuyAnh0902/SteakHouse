e1e866c5713202fbc89767899230dc10c42216a4
minhkhai10122003 <khainmhe171207@fpt.edu.vn>
2024-07-07 22:51:29 +0700

    Update VerifyCode.java

--- src/java/controller/LogIn/VerifyCode.java
+++ src/java/controller/LogIn/VerifyCode.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.LogIn;
 
 import dal.AccountDAO;
@@ -20,34 +19,37 @@ import model.Account;
  * @author ASUS
  */
 public class VerifyCode extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet VerifyCode</title>");  
+            out.println("<title>Servlet VerifyCode</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet VerifyCode at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet VerifyCode at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -55,12 +57,13 @@ public class VerifyCode extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -68,22 +71,29 @@ public class VerifyCode extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        HttpSession session = request.getSession(); 
-        Account user= (Account) session.getAttribute ("authcode");
-        String code = request.getParameter("authcode");
-        AccountDAO ac = new AccountDAO();
-        if (code.equals(user.getCode())) {
-            ac.addAccount(user);
-            request.setAttribute("RegisterSuccess", "Successfully registered");
-            request.getRequestDispatcher("user/login.jsp").forward(request, response);
-        }else{
-            response.sendRedirect("register");
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Account user = (Account) session.getAttribute("authcode");
+        if (user == null) {
+            request.setAttribute("verifyFail", "Please enter the OTP code again");
+            request.getRequestDispatcher("verify.jsp").forward(request, response);
+        } else {
+            AccountDAO ac = new AccountDAO();
+            String code = request.getParameter("authcode");
+            if (code.equals(user.getCode())) {
+                request.setAttribute("RegisterSuccess", "Successfully registered");
+                ac.addAccount(user);
+                request.getRequestDispatcher("user/login.jsp").forward(request, response);
+            } else {
+                request.setAttribute("verifyFail", "Please enter the OTP code again");
+                request.getRequestDispatcher("verify.jsp").forward(request, response);
+            }
         }
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
