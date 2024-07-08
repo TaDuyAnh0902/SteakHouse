f35336c081ffd5091a79736139ea8cb41581f0be
TaDuyAnh0902 <anhtdhe172879@fpt.edu.vn>
2024-07-09 01:00:34 +0700

    Update checkAdd.java

--- src/java/controller/manage/checkAdd.java
+++ src/java/controller/manage/checkAdd.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.manage;
 
 import dal.AccountDAO;
@@ -23,34 +22,37 @@ import model.Product;
  * @author ADMIN
  */
 public class checkAdd extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet checkAdd</title>");  
+            out.println("<title>Servlet checkAdd</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet checkAdd at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet checkAdd at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -58,38 +60,15 @@ public class checkAdd extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        AccountDAO acDAO = new AccountDAO();
-        String accountSearch = request.getParameter("accountSearch");
-        if (accountSearch != null) {
-            List<Account> list = acDAO.SearchAllAccount(accountSearch);
-            session.setAttribute("accountManage", list);
-            request.setAttribute("accountSize", list.size());
-        } else {
-            String cid = request.getParameter("cid");
-            String productSearch = request.getParameter("productSearch");
-            if (productSearch != null) {
-                ProductsDAO db = new ProductsDAO();
-                try {
-                    int cid_int = Integer.parseInt(cid);
-                    List<Product> list = db.searchProductByNameAndCid(productSearch, cid_int);
-                    request.setAttribute("products", list);
-                    request.setAttribute("productSize", list.size());
-
-                } catch (NumberFormatException e) {
-
-                }
-            } else {
-                request.setAttribute("productAdd", cid);
-            }
-        }
-
+            throws ServletException, IOException {
+        String cid = request.getParameter("cid");
+        request.setAttribute("productAdd", cid);
         request.getRequestDispatcher("home.jsp").forward(request, response);
-    } 
-    
-    /** 
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -97,7 +76,7 @@ public class checkAdd extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String name = request.getParameter("name");
         String quantity = request.getParameter("quantity");
         String price = request.getParameter("price");
@@ -121,8 +100,9 @@ public class checkAdd extends HttpServlet {
         response.sendRedirect("manageOption?check=productsManagement");
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
