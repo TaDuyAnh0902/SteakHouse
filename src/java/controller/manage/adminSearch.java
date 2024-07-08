16a38d8b99e480af33a46890103496bdb0aa0d8f
TaDuyAnh0902 <anhtdhe172879@fpt.edu.vn>
2024-07-09 01:00:02 +0700

    Create adminSearch.java

---
+++ src/java/controller/manage/adminSearch.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.manage;
+
+import dal.AccountDAO;
+import dal.ProductsDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import model.Account;
+import model.Product;
+
+/**
+ *
+ * @author Admin
+ */
+public class adminSearch extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet adminSearch</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet adminSearch at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        AccountDAO acDAO = new AccountDAO();
+        String accountSearch = request.getParameter("accountSearch");
+        if (accountSearch != null) {
+            List<Account> list = acDAO.SearchAllAccount(accountSearch);
+            session.setAttribute("accountManage", list);
+            request.setAttribute("accountSize", list.size());
+        } else {
+            String cid = request.getParameter("cid");
+            String productSearch = request.getParameter("productSearch");
+            ProductsDAO db = new ProductsDAO();
+            try {
+                int cid_int = Integer.parseInt(cid);
+                List<Product> list = db.searchProductByNameAndCid(productSearch, cid_int);
+                request.setAttribute("products", list);
+                request.setAttribute("productSize", list.size());
+
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        request.getRequestDispatcher("home.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
