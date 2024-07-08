f4b289be4486e2333a2e666219686c142ade7dc5
TaDuyAnh0902 <anhtdhe172879@fpt.edu.vn>
2024-07-09 01:00:46 +0700

    Update BlogDAO.java

--- src/java/dal/BlogDAO.java
+++ src/java/dal/BlogDAO.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dal;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,18 +12,20 @@ import java.util.List;
 import model.Account;
 import model.Blog;
 import model.Status;
+
 /**
  *
  * @author Admin
  */
-public class BlogDAO extends AccountDAO{
-    public List<Blog> getAllBlog(){
+public class BlogDAO extends AccountDAO {
+
+    public List<Blog> getAllBlog() {
         List<Blog> list = new ArrayList<>();
         String sql = "Select * from Blog";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
             ResultSet rs = st.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Blog b = new Blog();
                 b.setId(rs.getInt("id"));
                 b.setTitle(rs.getString("title"));
@@ -39,14 +42,14 @@ public class BlogDAO extends AccountDAO{
         }
         return list;
     }
-    
-    public Blog getBlogById(int id){
+
+    public Blog getBlogById(int id) {
         String sql = "select * from [Blog] where id = ?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 Blog b = new Blog();
                 b.setId(rs.getInt("id"));
                 b.setTitle(rs.getString("title"));
@@ -63,20 +66,33 @@ public class BlogDAO extends AccountDAO{
         }
         return null;
     }
-    
-    public Blog deleteBlogById(int id){
+
+    public Blog deleteBlogById(String id) {
         String sql = "Update [Blog] set sid = 2 where id = ?";
         try {
             PreparedStatement st = connection.prepareStatement(sql);
-            st.setInt(1, id);
+            st.setString(1, id);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
         return null;
     }
-    
-    public void addBlog(String title, String image, String source){
+
+    public void restoreTableById(String id) {
+        String sql = "Update [Blog] SET sid = 1 where id = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, id);
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+        }
+    }
+
+    public void addBlog(String title, String image, String source) {
         String sql = """
                      INSERT INTO [dbo].[Blog]
                                 ([title]
@@ -93,11 +109,11 @@ public class BlogDAO extends AccountDAO{
             st.setString(2, image);
             st.setString(3, source);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
     }
-    
+
     public void updateBlog(String title, String image, String source, int id) {
         String sql = """
                      UPDATE [dbo].[Blog]
@@ -112,7 +128,7 @@ public class BlogDAO extends AccountDAO{
             st.setString(3, source);
             st.setInt(4, id);
             st.executeUpdate();
-            
+
         } catch (SQLException e) {
         }
     }
