2f8fcb3b59577ce53eaed4fcaa358eebf8f47c96
TaDuyAnh0902 <anhtdhe172879@fpt.edu.vn>
2024-07-20 17:21:43 +0700

    Update orderlineAction.java

--- src/java/Order/orderlineAction.java
+++ src/java/Order/orderlineAction.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Order;
 
 import dal.OrderDAO;
@@ -12,40 +11,44 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.OrderLine;
 
 /**
  *
  * @author HP
  */
 public class orderlineAction extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet orderlineAction</title>");  
+            out.println("<title>Servlet orderlineAction</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet orderlineAction at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet orderlineAction at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -53,12 +56,13 @@ public class orderlineAction extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -66,16 +70,28 @@ public class orderlineAction extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-       String id = request.getParameter("id");
-        String quantity = request.getParameter("quantity");
+            throws ServletException, IOException {
         OrderDAO t = new OrderDAO();
+        String id = request.getParameter("id");
+        String pid = request.getParameter("pid");
+        OrderLine x = t.getOrderLineById(id);
+        int firstQuantity =0;
+        if(x.getSid().getId()==3){
+            firstQuantity = t.firstQuantity(id);
+        }
+        String quantity = request.getParameter("quantity");
         t.updateOrderLine(id, quantity);
+        if(x.getSid().getId()==3){
+            int quantityOfProduct = firstQuantity - Integer.parseInt(quantity);
+            t.updateQuantity(quantityOfProduct, pid);
+        }
+        
         response.sendRedirect("manageOrderAction?check=viewOrder");
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
